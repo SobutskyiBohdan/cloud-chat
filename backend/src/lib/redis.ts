be590fc1fb230eafc3efd88bff95c9755c9eaf87
SobutskyiBohdan <bodyaflorein@icloud.com>
2026-05-11 20:04:31 +0300

    Update redis.ts

--- backend/src/lib/redis.ts
+++ backend/src/lib/redis.ts
@@ -1,6 +1,6 @@
 import Redis from "ioredis";
 
-const REDIS_URL = process.env.REDIS_URL;
+const REDIS_URL = process.env.REDIS_URL!;
 if (!REDIS_URL) throw new Error("REDIS_URL environment variable is required");
 
 let _redis: Redis | null = null;
